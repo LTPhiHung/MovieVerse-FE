a70934e0dabe2614b1dd1c8ba61d48037966c07c
PhiHung1503 <phihung15032001@gmail.com>
2023-10-31 12:48:27 +0700

    create filter component and movies list for MoviesPgae

---
+++ src/Data/CategoriesData.js
@@ -0,0 +1,58 @@
+export const CategoriesData = [
+    {
+        _id: '1',
+        title: 'Action',
+    },
+    {
+        _id: '2',
+        title: 'Comedy',
+    },
+    {
+        _id: '3',
+        title: 'Drama',
+    },
+    {
+        _id: '4',
+        title: 'Adventure',
+    },
+    {
+        _id: '5',
+        title: 'Science Fiction',
+    },
+    {
+        _id: '6',
+        title: 'Horror',
+    },
+    {
+        _id: '7',
+        title: 'Romance',
+    },
+    {
+        _id: '8',
+        title: 'Fantasy',
+    },
+    {
+        _id: '9',
+        title: 'Mystery',
+    },
+    {
+        _id: '10',
+        title: 'Thriller',
+    },
+    {
+        _id: '11',
+        title: 'Animation',
+    },
+    {
+        _id: '12',
+        title: 'Family',
+    },
+    {
+        _id: '13',
+        title: 'Documentary',
+    },
+    {
+        _id: '14',
+        title: 'Biography',
+    },
+];
