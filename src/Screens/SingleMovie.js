b72dff65b6b3877c07ff568ab04f567020d0d060
PhiHung1503 <phihung15032001@gmail.com>
2023-10-31 21:17:07 +0700

    create RelatedMovies component for MovieDetail Pgae

--- src/Screens/SingleMovie.js
+++ src/Screens/SingleMovie.js
@@ -5,10 +5,14 @@ import { Movies } from '../Data/MovieData';
 import MovieInfo from '../Components/Single/MovieInfo';
 import MovieCasts from '../Components/Single/MovieCasts';
 import MovieRates from '../Components/Single/MovieRates';
+import Titles from '../Components/Titles';
+import { BsCollectionFill } from 'react-icons/bs';
+import Movie from '../Components/Movie';
 
 function SingleMovie() {
     const {id} = useParams();
     const movie = Movies.find((movie) => movie.name === id);
+    const RelatedMovies = Movies.filter((m) => m.category === movie.category);
   return (
     <Layout>
         <MovieInfo movie={movie} />
@@ -16,7 +20,17 @@ function SingleMovie() {
             <MovieCasts/>
             {/* rates */}
             <MovieRates movie={movie}/>
-            {/*  */}
+            {/* related */}
+            <div className='my-16'>
+                <Titles title="Related Movies" Icon={BsCollectionFill} />
+                <div className='grid sm:mt-10 mt-6 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-6'>
+                    {
+                        RelatedMovies.map((movie, index) => (
+                            <Movie key={index} movie={movie} />
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     </Layout>
   )
