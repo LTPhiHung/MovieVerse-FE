f0e75521613a6e2662ad83e9bedf4f7f39dc0698
PhiHung1503 <phihung15032001@gmail.com>
2023-11-02 00:11:46 +0700

    Finish popus for share, edit, reate popups for dashboard page and fix display top rated movies item responsive

--- src/Screens/SingleMovie.js
+++ src/Screens/SingleMovie.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Layout/Layout'
 import { useParams } from 'react-router-dom'
 import { Movies } from '../Data/MovieData';
@@ -8,14 +8,21 @@ import MovieRates from '../Components/Single/MovieRates';
 import Titles from '../Components/Titles';
 import { BsCollectionFill } from 'react-icons/bs';
 import Movie from '../Components/Movie';
+import ShareMovieModal from '../Modals/ShareModal';
 
 function SingleMovie() {
+    const [modalOpen, setModalOpen] = useState(false);
     const {id} = useParams();
     const movie = Movies.find((movie) => movie.name === id);
     const RelatedMovies = Movies.filter((m) => m.category === movie.category);
   return (
     <Layout>
-        <MovieInfo movie={movie} />
+        <ShareMovieModal
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+            movie={movie}
+        />
+        <MovieInfo movie={movie} setModalOpen={setModalOpen} />
         <div className='container mx-auto min-h-screen px-2 my-6'>
             <MovieCasts/>
             {/* rates */}
